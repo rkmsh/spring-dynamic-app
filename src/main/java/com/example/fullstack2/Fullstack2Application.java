864d6a816b2f59caf62ca5e2c00b8b02e54dd061
rkmsh <rohitshmahato@gmail.com>
2025-07-13 06:31:50 -0400

    initial commit

---
+++ src/main/java/com/example/fullstack2/Fullstack2Application.java
@@ -0,0 +1,13 @@
+package com.example.fullstack2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fullstack2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fullstack2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/fullstack2/app/AppController.java
@@ -0,0 +1,39 @@
+package com.example.fullstack2.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/contact")
+    public String contact(@RequestBody Contact contact, Model model) {
+        model.addAttribute("name", contact.name());
+        return "fragments/contact-success";
+    }
+
+
+
+
+}
+
+record Contact(String name, String email, String message) {}
